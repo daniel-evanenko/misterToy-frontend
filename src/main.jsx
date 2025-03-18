cf841097d8e197ce709ce13e20c89a4f2330077c
daniel <daniel.evanenko@gmail.com>
2025-03-18 22:41:55 +0200

    remove StrictMode

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './assets/style/App.scss'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  // <StrictMode>
+  <App />
+  // </StrictMode>,
 )
